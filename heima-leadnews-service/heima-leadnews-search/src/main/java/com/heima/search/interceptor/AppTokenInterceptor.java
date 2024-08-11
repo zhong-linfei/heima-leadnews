03346b2d6b108d60860a38ef2dd877b62cee188e
zhonglf <linfei.zhong@qq.com>
2024-08-11 20:28:04 +0800

    ES文章搜索，mongodb搜索保存

--- heima-leadnews-service/heima-leadnews-search/src/main/java/com/heima/search/interceptor/AppTokenInterceptor.java
+++ heima-leadnews-service/heima-leadnews-search/src/main/java/com/heima/search/interceptor/AppTokenInterceptor.java
@@ -1,6 +1,6 @@
+package com.heima.search.interceptor;
 
-package com.heima.wemedia.interceptor;
-
+import com.heima.model.user.pojos.ApUser;
 import com.heima.model.wemedia.pojos.WmUser;
 import com.heima.utils.thread.AppThreadLocalUtil;
 import com.heima.utils.thread.WmThreadLocalUtil;
@@ -11,9 +11,8 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Optional;
-
 @Slf4j
-public class WmTokenInterceptor implements HandlerInterceptor {
+public class AppTokenInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         //得到header中的信息
@@ -21,14 +20,15 @@ public class WmTokenInterceptor implements HandlerInterceptor {
         Optional<String> optional = Optional.ofNullable(userId);
         if(optional.isPresent()){
             //把用户id存入threadloacl中
-            WmUser wmUser = new WmUser();
-            wmUser.setId(Integer.valueOf(userId));
-            WmThreadLocalUtil.setUser(wmUser);
+            ApUser apUser = new ApUser();
+            apUser.setId(Integer.valueOf(userId));
+            AppThreadLocalUtil.setUser(apUser);
             log.info("wmTokenFilter设置用户信息到threadlocal中...");
         }
 
         return true;
     }
+
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         AppThreadLocalUtil.clear();
